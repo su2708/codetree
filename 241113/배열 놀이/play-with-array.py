cfb38876ba9b19c33ce7c3e4512a0f0fc785b551
su2708 <46125751+su2708@users.noreply.github.com>
2024-11-13 22:02:20 +0900

    Title: 배열 놀이, Time: 69ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 241113/배열 놀이/play-with-array.py
+++ 241113/배열 놀이/play-with-array.py
@@ -5,19 +5,24 @@ queries = [list(map(int, input().split())) for _ in range(q)]
 for idx, query in enumerate(queries):
     # 1 a
     if query[0] == 1:
-        print(nums[query[1]-1])
+        a = query[1]
+        print(nums[a-1])
     
     # 2 b
     elif query[0] == 2:
-        if query[1] not in nums:
+        b = query[1]
+        if b not in nums:
             print(0)
         else:
-            for i in nums:
-                if query[1] == i:
-                    print(nums.index(i)+1)
+            for i in range(len(nums)):
+                if nums[i] == b:
+                    print(i+1)
                     break
 
     # 3 s e
     else:
-        for i in range(query[1], query[2]+1):
-            print(nums[i-1], end=" ")
+        s = query[1]
+        e = query[2]
+        nums = nums[s-1:e]
+        for i in range(len(nums)):
+            print(nums[i], end=" ")
