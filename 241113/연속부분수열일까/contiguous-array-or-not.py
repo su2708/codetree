2f3eab8ecea99c0bc2fcebb5c3a9a10cda1d2e11
su2708 <46125751+su2708@users.noreply.github.com>
2024-11-13 22:57:40 +0900

    Title: 연속부분수열일까, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241113/연속부분수열일까/contiguous-array-or-not.py
+++ 241113/연속부분수열일까/contiguous-array-or-not.py
@@ -3,29 +3,24 @@ arr_a = list(map(int, input().split()))
 arr_b = list(map(int, input().split()))
 
 start = arr_b[0]
-indice = [idx for idx, val in enumerate(arr_a) if val==start]
 
-length = len(arr_b)
+len_a =len(arr_a)
+len_b = len(arr_b)
 arr_b.sort()
-cnt = len(indice)
 
-for idx in indice:
-    cnt -= 1
+for i in range(len_a):
     if start not in arr_a:
         print('No')
         break
-    elif len(arr_a[idx:]) < length:
+    elif len(arr_a[i:]) < len_b:
         print('No')
         break
     else:
-        arr = arr_a[idx:idx+length]
+        arr = arr_a[i:i+len_b]
         arr.sort()
         if arr == arr_b:
             print('Yes')
             break
         else:
-            if cnt!=0:
-                continue
-            else:
-                print('No')
-                break
+            arr_a = arr_a[i+1:]
+            continue
