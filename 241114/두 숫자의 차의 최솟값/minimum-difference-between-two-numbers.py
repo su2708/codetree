79b0665c44cd351a214ce43d1a6e9e7c088e7d77
su2708 <46125751+su2708@users.noreply.github.com>
2024-11-14 12:53:50 +0900

    Title: 두 숫자의 차의 최솟값, Time: 41ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241114/두 숫자의 차의 최솟값/minimum-difference-between-two-numbers.py
+++ 241114/두 숫자의 차의 최솟값/minimum-difference-between-two-numbers.py
@@ -7,8 +7,8 @@ min_dif = arr[1]-arr[0]
 idx_list = [0, 1]
 for idx, val in enumerate(arr[2:]):
     dif = val - min_val
-    if dif == 1:
-        print(dif)
+    if dif-min_dif == 1:
+        print(1)
         break
     elif dif-min_dif < min_dif:
         idx_list[0] = idx_list[1]
